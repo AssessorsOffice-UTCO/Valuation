030e5a4bc8aafbdfd15a89b5a937906cdf6fca10
Geoffrey Zahn <GeoffreyZ@utahcounty.gov>
2022-08-09 08:59:24 -0600

    add median estimate to find_cap_rate.R

--- R/find_cap_rate.R
+++ R/find_cap_rate.R
@@ -11,7 +11,7 @@
 #' net_income <- c(51329.93,76826.74,42861.05,34066.37,142085.22,81460.92)
 #' sale_price <- c(NA,1600000,0,1050000,1050000,479000)
 #' cap_rate <- find_cap_rate(net_income,sale_price)
-#' cap_rate$mean_estimate; cap_rate$mean_lowerbound; cap_rate$mean_upperbound
+#' cap_rate$mean_estimate; cap_rate$mean_lowerbound; cap_rate$mean_upperbound; cap_rate$median_estimate; cap_rate$median_lowerbound; cap_rate$median_upperbound
 #' @export
 
 # find cap rate based on property values
@@ -24,8 +24,17 @@ find_cap_rate <- function(net_income,sale_price){
   mean_estimate <- unname(meantest$estimate)
   mean_lowerbound <- unname(meantest$conf.int[1])
   mean_upperbound <- unname(meantest$conf.int[2])
+
+  median <- Valuation::MedianCI(cap_rate,na.rm=TRUE)
+  median_estimate <- unname(median)[1]
+  median_lowerbound <- unname(median)[1]
+  median_upperbound <- unname(median)[1]
+
   cap_rate <- list(mean_estimate=mean_estimate,
                    mean_lowerbound=mean_lowerbound,
-                   mean_upperbound=mean_upperbound)
+                   mean_upperbound=mean_upperbound,
+                   median_estimate=median_estimate,
+                   median_lowerbound=median_lowerbound,
+                   median_upperbound=median_upperbound)
   return(cap_rate)
 }
