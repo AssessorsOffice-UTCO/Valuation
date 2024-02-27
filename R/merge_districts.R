23d1d7d3502362bb8109c84fb4649c9c571f0711
DanielM <DanielM@UtahCounty.gov>
2024-02-27 12:05:09 -0700

    Update to test merge districts

--- R/merge_districts.R
+++ R/merge_districts.R
@@ -73,7 +73,7 @@ merge_districts <- function(df,
   small_districts_summaries$folded_district <- NA
   for(i in seq_along(small_districts_summaries$median_res_rcnld)){
     closest_large_district <-
-      large_districts_summaries$district[which.min(abs(small_districts_summaries$val_ratio[i]-large_districts_summaries$val_ratio))]
+      large_districts_summaries[,!! sym(district_id_col)][which.min(abs(small_districts_summaries$val_ratio[i]-large_districts_summaries$val_ratio))]
 
     if(length(closest_large_district) == 0){
       small_districts_summaries$folded_district[i] <- NA
@@ -91,74 +91,5 @@ merge_districts <- function(df,
 
   df <- left_join(df,new_districts,by=district_id_col)
 
-  return(df)
-}
-
-merge_districts2 <- function(df,
-                            N=50,
-                            district_id_col="district",
-                            sold_price_col="sold_price",
-                            res_rcnld_col="res_rcnld",
-                            rl_value_col="rl_value",
-                            acreage_col="acreage"){
-  # find cutoff for splitting "small" and "large" sales groups by land_district
-  sales_summary <- df %>%
-    group_by(!! sym(district_id_col)) %>% # !! sym() syntax allows string as quosure
-    summarize(N=n(), N_sales = sum(!is.na(!! sym(sold_price_col))))
-  
-  sales_cutoff <- N #setting arbitrary cutoff
-  
-  
-  
-  sales_summary <- sales_summary %>%
-    mutate(district_sales_count = case_when(N_sales >= sales_cutoff ~ "large",
-                                            TRUE ~ "small"))
-  df <- left_join(df,sales_summary,by=district_id_col)
-  
-  
-  # find closest res_rcnld from a large group and fold the smaller groups (districts) into those
-  small_districts_summaries <-
-    df %>%
-    dplyr::filter(district_sales_count == "small") %>%
-    group_by(!! sym(district_id_col)) %>%
-    summarize(mean_res_rcnld = mean(!! sym(res_rcnld_col),na.rm=TRUE),
-              median_res_rcnld = median(!! sym(res_rcnld_col),na.rm = TRUE),
-              sd_res_rcnld = sd(!! sym(res_rcnld_col),na.rm = TRUE),
-              val_ratio = median(!! sym(rl_value_col) / !! sym(acreage_col),na.rm=TRUE)) %>%
-    mutate(sales_volume = "small")
-  
-  large_districts_summaries <-
-    df %>%
-    dplyr::filter(district_sales_count == "large") %>%
-    group_by(!! sym(district_id_col)) %>%
-    summarize(mean_res_rcnld = mean(!! sym(res_rcnld_col),na.rm=TRUE),
-              median_res_rcnld = median(!! sym(res_rcnld_col),na.rm = TRUE),
-              sd_res_rcnld = sd(!! sym(res_rcnld_col),na.rm = TRUE),
-              val_ratio = median(!! sym(rl_value_col) / !! sym(acreage_col),na.rm=TRUE)) %>%
-    mutate(sales_volume = "large")
-  
-  # for each "small" district, find the "large" district that's most similar
-  # minimum difference between median costs per acre
-  small_districts_summaries$folded_district <- NA
-  for(i in seq_along(small_districts_summaries$median_res_rcnld)){
-    closest_large_district <-
-      large_districts_summaries$DISTRICT[which.min(abs(small_districts_summaries$val_ratio[i]-large_districts_summaries$val_ratio))]
-    
-    if(length(closest_large_district) == 0){
-      small_districts_summaries$folded_district[i] <- NA
-    } else {
-      small_districts_summaries$folded_district[i] <- closest_large_district
-    }
-  }
-  
-  large_districts_summaries <- large_districts_summaries %>%
-    mutate(new_district= !! sym(district_id_col))
-  small_districts_summaries <- small_districts_summaries %>%
-    mutate(new_district = folded_district)
-  new_districts <- full_join(large_districts_summaries,small_districts_summaries) %>%
-    select(!! sym(district_id_col),new_district)
-  
-  df <- left_join(df,new_districts,by=district_id_col)
-  
   return(df)
 }
